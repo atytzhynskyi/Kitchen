833bd2ca04006ea5a4c1c2b17dd2d9071d9c96ac
atytzhynskyi <atytzhynskyi@future-processing.com>
2021-09-10 10:18:42 +0300

    Add project files.

---
+++ Chiken Kitchen/Food.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chiken_Kitchen
+{
+    class Food : Ingredient
+    {
+        public List<Ingredient> Recipe = new List<Ingredient>();
+        public override List<Ingredient> GetRecipe() => Recipe;
+
+        public Food(string _Name, params Ingredient[] _Recipe) : base(_Name)
+        {
+            Recipe.AddRange(_Recipe);
+        }
+        public Food(string _Name, int _Count, params Ingredient[] _Recipe) : base(_Name, _Count)
+        {
+            Recipe.AddRange(_Recipe);
+        }
+        public override void Cook(List<Ingredient> allIngredients)
+        {
+            if (!isEnoughIngredients(allIngredients))
+            {
+                return;
+            }
+            CookWithoutCheck(allIngredients);
+        }
+        public override void CookWithoutCheck(List<Ingredient> allIngredients)
+        {
+            foreach (Ingredient ingredient in allIngredients)
+                foreach (Ingredient ingredientRecipe in Recipe)
+                {
+                    if (ingredientRecipe.Name == ingredient.Name)
+                    {
+                        while (ingredient.GetRecipe().Count != 0 && ingredient.Count < ingredientRecipe.Count)
+                        {
+                            ingredient.CookWithoutCheck(allIngredients);
+                        }
+                        ingredient.UseIngredient(allIngredients);
+                    }
+                }
+            Count++;
+        }
+        public override bool isEnoughIngredients(List<Ingredient> allIngredients)
+        {
+            List<int> startValues = new List<int>();
+            foreach (Ingredient ingredient in allIngredients)
+            {
+                startValues.Add(ingredient.Count);
+            }
+
+            CookWithoutCheck(allIngredients);
+            List<Ingredient> missingIngredients = new List<Ingredient>();
+            foreach (Ingredient ingredient in allIngredients)
+            {
+                foreach (Ingredient ingredientRecipe in GetRecipe())
+                    if (ingredient.Count < 0 && ingredient.Name==ingredientRecipe.Name)
+                    {
+                        missingIngredients.Add(ingredient);
+                    }
+            }
+            if (missingIngredients.Count != 0)
+            {
+                foreach (Ingredient ingredient in missingIngredients)
+                {
+                    Console.WriteLine("We dont have enough " + ingredient.Name);
+                }
+                SetStartCount(allIngredients, startValues);
+                return false;
+            }
+            SetStartCount(allIngredients, startValues);
+            return true;
+        }
+        private void SetStartCount(List<Ingredient> allIngredients, List<int> StartValues)
+        {
+            int i = 0;
+            foreach (Ingredient ingredient in allIngredients)
+            {
+                ingredient.Count = StartValues[i];
+                i++;
+            }
+        }
+        static public bool isAllergiesFood(List<Ingredient> allIngredients, Ingredient food, List<Ingredient> customerAllergies)
+        {
+            foreach (Ingredient ingredient in allIngredients) //Set ingredient from allIngredient as customerOrder to set a Recipe
+                if (food.Name == ingredient.Name)
+                {
+                    food = ingredient;
+                    break;
+                }
+            foreach (Ingredient ingredient in food.GetRecipe())
+            {
+                foreach (Ingredient allergiesIngredient in customerAllergies)
+                    if (ingredient.Name == allergiesIngredient.Name)
+                    {
+                        return true;
+                    }
+                if (isAllergiesFood(allIngredients, ingredient, customerAllergies)) return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Chiken Kitchen/Сustomer.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chiken_Kitchen
+{
+    class Customer
+    {
+        public string Name;
+        public Ingredient Order = new Ingredient("");
+        public List<Ingredient> Allergies = new List<Ingredient>();
+        public Customer(string _Name, params Ingredient[] _Allergies)
+        {
+            Name = _Name;
+            Allergies.AddRange(_Allergies);
+        }
+        public Customer(string _Name)
+        {
+            Name = _Name;
+        }
+        public void SetOrder(Ingredient _Order, List<Ingredient> allIngredients)
+        {
+            foreach(Ingredient ingredient in allIngredients)
+            {
+                if (_Order.Name == ingredient.Name)
+                {
+                    Order.Name = ingredient.Name;
+                    Order.Count = 1;
+                }
+            }
+        }
+        public void SetOrder(Ingredient _Order, List<Ingredient> allIngredients, int OrderCount)
+        {
+            foreach (Ingredient ingredient in allIngredients)
+            {
+                if (_Order.Name == ingredient.Name)
+                {
+                    Order.Name = ingredient.Name;
+                    Order.Count = OrderCount;
+                }
+            }
+        }
+        public void Service(List<Ingredient> allIngredients)
+        {
+            if (Food.isAllergiesFood(allIngredients, Order, Allergies))
+            {
+                Console.WriteLine(Order.Name + " is allergic food for " + Name);
+            }
+            else
+            {
+                foreach (Ingredient ingredient in allIngredients)
+                {
+                    if(ingredient.Name == Order.Name)
+                    {
+                        ingredient.Cook(allIngredients);
+                    }
+                }
+                GiveFood(allIngredients);
+            }
+        }
+        public void GiveFood(List<Ingredient> allIngredients)
+        {
+            foreach (Ingredient ingredient in allIngredients)
+            {
+                if (ingredient.Name == Order.Name)
+                {
+                    if (ingredient.Count < Order.Count)
+                    {
+                        Console.WriteLine("We dont have " +Order.Name);
+                        return;
+                    }
+                    ingredient.Count-=Order.Count;
+                    Console.WriteLine(Name + " get " + ingredient.Name);
+                    return;
+                }
+            }
+            Console.WriteLine("Order doesnt exist in Ingedient List");
+        }
+        
+    }
+}
